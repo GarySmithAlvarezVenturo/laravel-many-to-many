2cb73cc34a6cb4f7e9fefea4bae0e60819e68d88
GarySmithAlvarezVenturo <Garyalvarez@outlook.it>
2023-07-12 17:21:35 +0200

    fix name seeder

--- resources/views/Admin/includes/nav.blade.php
+++ resources/views/Admin/includes/nav.blade.php
@@ -2,12 +2,11 @@
     $user = Auth::user();
 @endphp
 
-<nav class="navbar navbar-expand-lg bg-body-tertiary">
-    <div class="container-fluid">
-        <a class="navbar-brand" href="http://127.0.0.1:8000/">Boolfolio</a>
+<nav>
+    <div class="container d-flex justify-content-between">
 
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <div class="navigation">
+            <ul class="d-flex list-unstyled py-3 gap-2">
                 <li class="nav-item">
                     <a class="nav-link" href="{{ route('admin.dashboard') }}">Dashboard</a>
                 </li>
@@ -15,7 +14,7 @@
                     <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                         Projects
                     </a>
-                    <ul class="dropdown-menu">
+                    <ul class="dropdown-menu dropdown-menu-dark">
                         <li><a class="dropdown-item" href="{{ route('admin.project.index') }}">Index</a></li>
                         <li><a class="dropdown-item" href="{{ route('admin.project.create') }}">Add</a></li>
                     </ul>
@@ -25,19 +24,17 @@
                     <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                         Type
                     </a>
-                    <ul class="dropdown-menu">
+                    <ul class="dropdown-menu dropdown-menu-dark">
                         <li><a class="dropdown-item" href="{{ route('admin.type.index') }}">Index</a></li>
                         <li><a class="dropdown-item" href="{{ route('admin.type.create') }}">Add</a></li>
                     </ul>
-    
                 </li>
-
 
                 <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                         Languages
                     </a>
-                    <ul class="dropdown-menu">
+                    <ul class="dropdown-menu dropdown-menu-dark">
                         <li><a class="dropdown-item" href="{{ route('admin.language.index') }}">Index</a></li>
                         <li><a class="dropdown-item" href="{{ route('admin.language.create') }}">Add</a></li>
                     </ul>
@@ -47,11 +44,12 @@
                     <a class="nav-link" href="{{ route('admin.project.trashed') }}" role="button" aria-expanded="false">
                         Bin
                     </a>
-                </li>
+                </li> 
             </ul>
+        </div>
 
-            
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <div class="profile">
+            <ul class="d-flex list-unstyled py-3 gap-2">
                 <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                         {{$user->name }}
@@ -68,11 +66,9 @@
                     </ul>
                 </li>
             </ul>
-
-            {{-- <form class="d-flex" role="search">
-                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                <button class="btn btn-outline-success" type="submit">Search</button>
-            </form> --}}
         </div>
+
     </div>
 </nav>
+
+
